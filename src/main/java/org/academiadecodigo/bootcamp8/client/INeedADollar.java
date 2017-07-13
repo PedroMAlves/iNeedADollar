806fcca927ee6aa216b6b641c4241674f7f638bd
Pedro <drpedro.psi@gmail.com>
2017-07-13 16:13:51 +0100

    pom.xml configured

---
+++ src/main/java/org/academiadecodigo/bootcamp8/client/INeedADollar.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.bootcamp8.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Prashanta on 13/07/17.
+ */
+public class INeedADollar extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
+}
---
+++ src/main/java/org/academiadecodigo/bootcamp8/server/ServerStart.java
@@ -0,0 +1,11 @@
+package org.academiadecodigo.bootcamp8.server;
+
+/**
+ * Created by Prashanta on 13/07/17.
+ */
+public class ServerStart {
+
+    public static void main(String[] args) {
+
+    }
+}
