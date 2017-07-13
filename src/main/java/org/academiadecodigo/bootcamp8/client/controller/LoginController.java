ea0c1fe8e3a3457554caade1e14aa0d18cec0ddf
Tiago Santos <tiago924santos@gmail.com>
2017-07-13 18:28:40 +0100

    Worked on LoginController

--- src/main/java/org/academiadecodigo/bootcamp8/client/controller/LoginController.java
+++ src/main/java/org/academiadecodigo/bootcamp8/client/controller/LoginController.java
@@ -1,12 +1,23 @@
 package org.academiadecodigo.bootcamp8.client.controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class LoginController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable{
+
+    @FXML
+    private VBox box;
 
     @FXML
     private GridPane loginGrid;
@@ -25,5 +36,37 @@ public class LoginController {
 
     @FXML
     private Hyperlink help;
+
+    private Stage stage;
+
+    private double x;
+    private double y;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setDraggable();
+    }
+
+    private void setDraggable() {
+
+        box.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                x = event.getSceneX();
+                y = event.getSceneY();
+            }
+        });
+
+        box.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - x);
+                stage.setY(event.getScreenY() - y);
+            }
+        });
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 
 }
