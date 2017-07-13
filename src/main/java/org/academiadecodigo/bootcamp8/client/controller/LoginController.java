986d0320b80ee710dc74adcb6177962b102d9862
Pedro <drpedro.psi@gmail.com>
2017-07-13 16:17:33 +0100

    basic packages

---
+++ src/main/java/org/academiadecodigo/bootcamp8/client/controller/LoginController.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp8.client.controller;
+
+/**
+ * Created by Prashanta on 13/07/17.
+ */
+public class LoginController {
+}
---
+++ src/main/java/org/academiadecodigo/bootcamp8/client/service/LoginService.java
@@ -0,0 +1,7 @@
+package org.academiadecodigo.bootcamp8.client.service;
+
+/**
+ * Created by Prashanta on 13/07/17.
+ */
+public class LoginService {
+}
