2d63fe8a75cacdbe060fb7212908bb4813eedcbb
jpmramos <jpmramos@gmail.com>
2017-07-14 04:03:59 +0100

    database schema updated

--- src/main/resources/dollar.sql
+++ src/main/resources/dollar.sql
@@ -20,43 +20,38 @@ CREATE TABLE bio (
 );
 
 
-CREATE TABLE account  ( 
+CREATE TABLE account  (
   user_account	CHAR(15)  NOT NULL,
-  balance		INTEGER  , 	
+  balance		INTEGER  ,
   PRIMARY KEY (user_account),
   FOREIGN KEY (user_account) REFERENCES user(user_name) ON DELETE CASCADE
 );
 
 
 CREATE TABLE request (
-  request_id 			INTEGER AUTO_INCREMENT, 
+  request_id 			INTEGER AUTO_INCREMENT,
   user_name_request 	CHAR(15) NOT NULL,
   user_name_answer		CHAR(15),
   operation_id			INTEGER ,
   request_motiv 		CHAR(50),
   request_answer		CHAR(50),
-  state 				CHAR(10), 
+  state 				CHAR(10),
   date_request 			DATE,
   PRIMARY KEY (request_id, user_name_request),
   FOREIGN KEY (user_name_request) REFERENCES user (user_name) ON DELETE CASCADE
 );
 
 CREATE TABLE operation (
-  operation_id 	INTEGER AUTO_INCREMENT, 
-  request_id 		INTEGER NOT NULL,
+  operation_id 	INTEGER AUTO_INCREMENT,
+  request_id 		INTEGER NOT NULL UNIQUE,
   user_account		CHAR(15) NOT NULL,
   amount     INTEGER  NOT NULL,
   type 		CHAR(15) NOT NULL,
   PRIMARY KEY (operation_id, request_id, user_account),
   FOREIGN KEY (user_account) REFERENCES account(user_account) ON DELETE CASCADE,
-  FOREIGN KEY (request_id) REFERENCES request (request_id) 
+  FOREIGN KEY (request_id) REFERENCES request (request_id)
+
 );
 
 
 ALTER TABLE request ADD FOREIGN KEY (operation_id) REFERENCES operation (operation_id);
-
-
-
-
-
-
